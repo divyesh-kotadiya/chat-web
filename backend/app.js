0833eb94a92dfbb7a120940aec170795b74ba025
divyesh-kotadiya <dvs.kotadiya@gmail.com>
2025-11-10 11:41:20 +0530

    feat: add initial project and environment setup

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./utils/errorController");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const userRouter = require("./routes/userRouter");
+const chatRouter = require("./routes/chatRouter");
+const messageRouter = require("./routes/messageRouter");
+
+
+const allowedOrigins = ['http://localhost:3000','https://chat-box-samarthkadam.vercel.app'];
+app.use(cors({
+  origin:'*'
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public/img/user'));
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chat", chatRouter);
+app.use("/api/v1/message", messageRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError("TypeError", 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
